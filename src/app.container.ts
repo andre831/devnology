033cd66538d4b901c5f7e1430ef14ea83b721349
Andre Santos <andredupdm@gmail.com>
2023-01-25 14:46:56 -0300

    start e-commerce front-end

---
+++ src/app.container.ts
@@ -0,0 +1,6 @@
+import { container } from "inversify-props";
+import "reflect-metadata";
+
+export default function buildDependencyContainer(): void {
+  // container.addTransient<IEffectsService>(EffectsService);
+}
