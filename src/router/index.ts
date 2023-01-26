5d5ddac607c8b952db3f282f9c7c22774e574548
Andre Andre Andre <andredupdm@gmail.com>
2023-01-25 23:58:18 -0300

    quick start in routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
-import Vue from "vue";
+import { Vue } from "vue-property-decorator";
 import VueRouter, { RouteConfig } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import CheckoutView from "@/views/CheckoutView.vue";
+import UserView from "@/views/UserView.vue";
 
 Vue.use(VueRouter);
 
@@ -8,16 +9,16 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/checkout",
+    name: "checkout",
+    component: CheckoutView,
+  },
+  {
+    path: "/user",
+    name: "user",
+    component: UserView,
   },
 ];
 
