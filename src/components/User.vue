1a1d435cf00e851b014a69228cb88a2866c42a82
Andre Santos <andredupdm@gmail.com>
2023-01-30 22:31:00 -0300

    feat: change folder position

---
+++ src/components/User.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="$router.push('/user')">
+    <i class="fa-solid fa-user"></i>
+  </button>
+</template>
+
+<style lang="scss" scoped></style>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class User extends Vue {}
+</script>
