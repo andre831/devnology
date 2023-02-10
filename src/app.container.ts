090c8c9e42b2070dd63aa382406ec9607769d24f
Andre Andre Andre <andredupdm@gmail.com>
2023-02-10 07:35:49 -0300

    feat: add iformat service

--- src/app.container.ts
+++ src/app.container.ts
@@ -4,8 +4,11 @@ import IValidateService from "./services/ValidateService/IValidateService";
 import ValidateService from "./services/ValidateService/ValidateService";
 import IProductsService from "./services/ProductsService/IProductsService";
 import ProductsService from "./services/ProductsService/ProductsService";
+import IFormaterService from "./services/FormaterService/IFormaterService";
+import FormatService from "./services/FormaterService/FormaterService";
 
 export default function buildDependencyContainer(): void {
   container.addTransient<IValidateService>(ValidateService);
   container.addTransient<IProductsService>(ProductsService);
+  container.addTransient<IFormaterService>(FormatService);
 }
